fc201e2f24beddad23af2f8f5a1dc4d3b2032a4f
James Bray <47334864+James-Bray19@users.noreply.github.com>
2023-08-11 16:42:18 +0100

    Update ObjectiveFunction.m

--- ObjectiveFunction.m
+++ ObjectiveFunction.m
@@ -1,14 +1,14 @@
 function value = ObjectiveFunction(Kp, Ki, Kd, G, t)
+
     % perform a unity feedback step input response
     C = tf([Kd Kp Ki], [0 1 0]);
     CG = C*G;
     y = step(CG/(1+CG), t);
 
     % performance evaluation formulae
-    % 
-
     % value = trapz(t, y.^2);        %ISE
     value = trapz(t, abs(y));      %IAE
     % value = trapz(t, (y.^2) .* t); %ITSE
     % value = trapz(t, abs(y) .* t); %ITAE
+    
 end
